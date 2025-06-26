5b1526b172601a4248d0e60a38adf37b5711b5a5
Asadullo-17 <abduvohidovasadullo1707@gmail.com>
2025-06-26 15:51:22 +0500

    Create 6-task.sql

---
+++ Tasks/6-task.sql
@@ -0,0 +1,19 @@
+SELECT 
+    DISTINCT c.CustomerID,
+    c.FullName,
+    t1.TransactionID AS Transaction1,
+    t2.TransactionID AS Transaction2,
+    t1.Amount AS Amount1,
+    t2.Amount AS Amount2,
+    t1.Date AS Date1,
+    t2.Date AS Date2
+FROM Core_Banking.Customers c
+JOIN Core_Banking.Accounts as a1 on c.CustomerID=a1.CustomerID
+JOIN Core_Banking.Transactions as t1 on a1.AccountID=t1.AccountID
+JOIN Core_Banking.Accounts as a2 on a2.CustomerID=c.CustomerID
+JOIN Core_Banking.Transactions as t2 on a2.AccountID=t2.AccountID
+WHERE
+	t1.TransactionID <> t2.TransactionID
+    AND t1.Amount>10000
+	AND t2.Amount>10000
+    AND ABS(DATEDIFF(MINUTE, t1.Date, t2.Date)) <= 60;
